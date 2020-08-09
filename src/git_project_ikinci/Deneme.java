ee4c815eda1c303d208ed54921d19a2676e4166b
huseyin0865 <69403340+huseyin0865@users.noreply.github.com>
2020-08-09 22:19:23 +0200

    Update Deneme.java
    gutHub da degisiklik yaptim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("ikinci git projem");
 		System.out.println("ikinci satir kodum");
+		Sistem.out.println("remote degisiklik");
 
 	}
 
